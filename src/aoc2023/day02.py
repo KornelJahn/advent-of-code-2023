6fcd0cb748b365b987d55b38e743f5417999fbcd
Kornel Jahn <kjahn.public@gmail.com>
2023-12-02 20:10:26 +0100

    Solve day02.

--- src/aoc2023/day02.py
+++ src/aoc2023/day02.py
@@ -1,10 +1,25 @@
+import math
+import re
+
 def parse_input(raw_input):
-    return None
+    games = []
+    for line in raw_input.split("\n"):
+        draws = re.findall(r"([0-9]+) ([a-z]+)", line.split(": ")[1])
+        games.append({
+            color: max(int(v) for v, k in draws if k == color)
+            for color in ("red", "green", "blue")
+        })
+    return games
 
 
-def solve_part1(input_):
-    return None
+def solve_part1(games):
+    return sum(
+        i for i, max_counts in enumerate(games, start=1)
+        if max_counts["red"] <= 12
+        and max_counts["green"] <= 13
+        and max_counts["blue"] <= 14
+    )
 
 
-def solve_part2(input_):
-    return None
+def solve_part2(games):
+    return sum(math.prod(max_counts.values()) for max_counts in games)
--- tests/day02_test.py
+++ tests/day02_test.py
@@ -3,17 +3,17 @@ from aoc2023.day02 import parse_input, solve_part1, solve_part2
 
 
 def test_part1():
-    assert solve_part1(EXAMPLE_INPUT) == None
-    # assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(EXAMPLE_INPUT) == 8
+    assert solve_part1(PUZZLE_INPUT) == 2369
 
 
 def test_part2():
-    assert solve_part2(EXAMPLE_INPUT) == None
-    assert solve_part2(PUZZLE_INPUT) == None
+    assert solve_part2(EXAMPLE_INPUT) == 2286
+    assert solve_part2(PUZZLE_INPUT) == 66363
 
 
 PUZZLE_INPUT = parse_input(
-    (Path(__file__).parent / "data" / "day02.txt").read_text()
+    (Path(__file__).parent / "data" / "day02.txt").read_text().strip()
 )
 
 EXAMPLE_INPUT = parse_input("""\
@@ -21,5 +21,4 @@ Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-""")
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green""")
