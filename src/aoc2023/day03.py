e8fe136682f035236dbf0101ab3a4c767e498c94
Kornel Jahn <kjahn.public@gmail.com>
2023-12-03 08:42:37 +0100

    Solve day03.

--- src/aoc2023/day03.py
+++ src/aoc2023/day03.py
@@ -1,10 +1,47 @@
+import math
+import re
+
+
 def parse_input(raw_input):
-    return None
+    line_length = raw_input.index("\n")
+    numbers = [
+        (
+            int(m.group()),
+            to_row_col_indices(m.start(0), line_length + 1),
+            len(m.group())
+        )
+        for m in re.finditer(r"(\d+)", raw_input)
+    ]
+    symbols = [
+        (
+            m.group(),
+            to_row_col_indices(m.start(0), line_length + 1)
+        )
+        for m in re.finditer(r"([^\d\.\r\n])", raw_input)
+    ]
+    return numbers, symbols
 
 
 def solve_part1(input_):
-    return None
+    numbers, symbols = input_
+    return sum(n[0] for n in numbers if any(adjacent(n, s) for s in symbols))
 
 
 def solve_part2(input_):
-    return None
+    numbers, symbols = input_
+    return sum(gear_ratio(s, numbers) for s in symbols if s[0] == "*")
+
+
+def to_row_col_indices(flat_index, line_length):
+    return flat_index // line_length, flat_index % line_length
+
+
+def adjacent(number, symbol):
+    _, (i, j), n = number
+    _, (p, q) = symbol
+    return i-1 <= p <= i+1 and j-1 <= q <= j+n
+
+
+def gear_ratio(symbol, numbers):
+    neighbors = [n[0] for n in numbers if adjacent(n, symbol)]
+    return math.prod(neighbors) if len(neighbors) == 2 else 0
--- tests/day03_test.py
+++ tests/day03_test.py
@@ -4,12 +4,12 @@ from aoc2023.day03 import parse_input, solve_part1, solve_part2
 
 def test_part1():
     assert solve_part1(EXAMPLE_INPUT) == 4361
-    # assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(PUZZLE_INPUT) == 498559
 
 
-# def test_part2():
-#     assert solve_part2(EXAMPLE_INPUT) == None
-#     assert solve_part2(PUZZLE_INPUT) == None
+def test_part2():
+    assert solve_part2(EXAMPLE_INPUT) == 467835
+    assert solve_part2(PUZZLE_INPUT) == 72246648
 
 
 PUZZLE_INPUT = parse_input(
