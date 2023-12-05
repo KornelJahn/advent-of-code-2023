7a7fc289a081032f5b93f42590d640b0a842fd2f
Kornel Jahn <kjahn.public@gmail.com>
2023-12-05 11:51:32 +0100

    Add day05 skeleton.

---
+++ tests/day05_test.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from aoc2023.day05 import parse_input, solve_part1, solve_part2
+
+
+def test_part1():
+    assert solve_part1(EXAMPLE_INPUT) == 35
+    # assert solve_part1(PUZZLE_INPUT) == None
+
+
+def test_part2():
+    assert solve_part2(EXAMPLE_INPUT) == None
+    assert solve_part2(PUZZLE_INPUT) == None
+
+
+PUZZLE_INPUT = parse_input(
+    (Path(__file__).parent / "data" / "day05.txt").read_text()
+)
+
+EXAMPLE_INPUT = parse_input("""\
+""")
