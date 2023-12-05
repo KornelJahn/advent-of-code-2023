e484c8e1c9ae2ea8b665391de626084fef7a74d7
Kornel Jahn <kjahn.public@gmail.com>
2023-12-05 09:34:33 +0100

    Solve day04 naively.

--- src/aoc2023/day04.py
+++ src/aoc2023/day04.py
@@ -1,10 +1,38 @@
+from collections import defaultdict
+
+
 def parse_input(raw_input):
-    return None
+    lines = raw_input.strip().split("\n")
+    cards = [
+        [group.split() for group in line.split(": ")[1].split(" | ")]
+        for line in lines
+    ]
+    win_counts = [
+        len(set(winning).intersection(mine)) for winning, mine in cards
+    ]
+    return win_counts
 
 
-def solve_part1(input_):
-    return None
+def solve_part1(win_counts):
+    return sum(points(win_count) for win_count in win_counts)
 
 
-def solve_part2(input_):
-    return None
+def solve_part2(win_counts):
+    tree = [len(win_counts)] + win_counts
+    return sum(1 for node in preorder_dfs(tree))
+
+
+def points(count):
+    return 2**(count - 1) if count > 0 else 0
+
+
+def preorder_dfs(tree):
+    for node in children(tree):
+        yield node
+        yield from preorder_dfs(node)
+
+
+def children(node):
+    if not node or node[0] == 0:
+        return []
+    return [node[i:] for i in range(1, node[0] + 1)]
--- tests/day04_test.py
+++ tests/day04_test.py
@@ -3,13 +3,13 @@ from aoc2023.day04 import parse_input, solve_part1, solve_part2
 
 
 def test_part1():
-    assert solve_part1(EXAMPLE_INPUT) == None
-    assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(EXAMPLE_INPUT) == 13
+    assert solve_part1(PUZZLE_INPUT) == 18653
 
 
 def test_part2():
-    assert solve_part2(EXAMPLE_INPUT) == None
-    assert solve_part2(PUZZLE_INPUT) == None
+    assert solve_part2(EXAMPLE_INPUT) == 30
+    assert solve_part2(PUZZLE_INPUT) == 5921508
 
 
 PUZZLE_INPUT = parse_input(
@@ -17,4 +17,9 @@ PUZZLE_INPUT = parse_input(
 )
 
 EXAMPLE_INPUT = parse_input("""\
-""")
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11""")
