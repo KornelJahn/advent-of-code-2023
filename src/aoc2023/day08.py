159e51ce0ec6a4c130c10bd4e97815899cf38cb0
Kornel Jahn <kjahn.public@gmail.com>
2023-12-08 15:33:33 +0100

    day08: Explicitly state assumptions via asserts in part 2.

--- src/aoc2023/day08.py
+++ src/aoc2023/day08.py
@@ -21,33 +21,31 @@ def solve_part1(input_):
 
 def solve_part2(input_):
     directions, network = input_
-    counts_to_first_stop = []
-    first_stops = []
-    for start in [node for node in network.keys() if node.endswith("A")]:
-        iterator = walk(
-            directions,
-            network,
-            start,
-            stop_pred=lambda s: s.endswith("Z")
-        )
-        for count, node in enumerate(iterator, start=1):
-            pass
-        counts_to_first_stop.append(count)
-        first_stops.append(node)
+    nodes = [[node for node in network.keys() if node.endswith("A")]]
+    counts = []
+    for i in range(2):
+        counts.append([])
+        nodes.append([])
 
-    period_counts = []
-    for start in first_stops:
-        iterator = walk(
-            directions,
-            network,
-            start,
-            stop_pred=lambda s: s.endswith("Z")
-        )
-        period_counts.append(sum(1 for _ in iterator))
+        for node in nodes[-2]:
+            iterator = walk(
+                directions,
+                network,
+                start=node,
+                stop_pred=lambda s: s.endswith("Z")
+            )
+            for count, final_node in enumerate(iterator, start=1):
+                pass
+            counts[-1].append(count)
+            nodes[-1].append(final_node)
 
-    assert counts_to_first_stop == period_counts
+        if i > 0:
+            # Check if we can assume that the counts have a fixed period
+            assert counts[-1] == counts[-2]
+            # Check if starting from a final node returns to the same one
+            assert nodes[-1] == nodes[-2]
 
-    return math.lcm(*period_counts)
+    return math.lcm(*counts[-1])
 
 
 def walk(directions, network, start, stop_pred):
