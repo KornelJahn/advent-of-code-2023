3f289cc1bde3e9d2defb7a5bed890c76e89234dc
Kornel Jahn <kjahn.public@gmail.com>
2023-12-09 13:51:37 +0100

    day09.py: Revise regex and formatting.

--- src/aoc2023/day09.py
+++ src/aoc2023/day09.py
@@ -4,7 +4,7 @@ def parse_input(raw_input):
     lines = raw_input.strip().split("\n")
 
     def convert(line):
-        return [int(d) for d in re.findall(r"([-\d]+)", line)]
+        return [int(d) for d in re.findall(r"(-?\d+)", line)]
 
     return list(map(convert, lines))
 
@@ -36,14 +36,15 @@ def solve_part2(multi_series):
 #
 # In summary, the extrapolated value is simply the sum of diagonal elements.
 
-# # Tail-recursive version
+
 # def extrapolate(xs, acc=0):
 #     if all(x == 0 for x in xs):
 #         return acc
 #     else:
 #         return extrapolate([y - x for y, x in zip(xs[1:], xs)], acc + xs[-1])
 
-# Manually tail-call optimized version
+
+# Manual tail-call optimization
 def extrapolate(xs):
     acc = 0
     while True:
