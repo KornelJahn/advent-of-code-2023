9a720826360341f7ef5c5811429b474572e96e21
Kornel Jahn <kjahn.public@gmail.com>
2023-12-09 12:42:25 +0100

    Solve day09.

--- src/aoc2023/day09.py
+++ src/aoc2023/day09.py
@@ -1,10 +1,46 @@
+import re
+
 def parse_input(raw_input):
-    return None
+    lines = raw_input.strip().split("\n")
+
+    def convert(line):
+        return [int(d) for d in re.findall(r"([-\d]+)", line)]
+
+    return list(map(convert, lines))
 
 
-def solve_part1(input_):
-    return None
+def solve_part1(multi_series):
+    return sum(extrapolated_value(xs) for xs in multi_series)
 
 
-def solve_part2(input_):
-    return None
+def solve_part2(multi_series):
+    return sum(extrapolated_value(list(reversed(xs))) for xs in multi_series)
+
+
+# Recurrence relation for an arbitrary element of the triangle:
+# a[i,j] = a[i-1,j+1] - a[i-1, j]
+#
+# Diagonal elements:
+# a[0,n] = a[1,n-1] + a[0,n-1]
+# a[1,n-1] = a[2,n-2] + a[1,n-2]
+# a[2,n-2] = a[3,n-3] + a[2,n-3]
+# ...
+# a[n,0] = 0 (by definition)
+#
+# Expanding all intermediate extrapolated elements a[i,n-i]:
+# a[0,n] = a[2,n-2] + a[1,n-2] + a[0,n-1]
+#        = a[3,n-3] + a[2,n-3] + a[1,n-2] + a[0,n-1]
+#        = ...
+#        = a[n,0] + a[n-1,0] + a[n-2,1] + a[n-3,2] + ...
+#        = a[n-1,0] + a[n-2,1] + a[n-3,2] + ...
+#
+# In summary, the extrapolated value is simply the sum of diagonal elements.
+
+def extrapolated_value(xs):
+    triangle = []
+    for _ in range(len(xs)):
+        triangle.append(xs)
+        xs = [y - x for y, x in zip(xs[1:], xs)]
+        if all(x == 0 for x in xs):
+            break
+    return sum(row[-1] for row in triangle)
--- tests/day09_test.py
+++ tests/day09_test.py
@@ -3,13 +3,13 @@ from aoc2023.day09 import parse_input, solve_part1, solve_part2
 
 
 def test_part1():
-    assert solve_part1(EXAMPLE_INPUT) == None
-    assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(EXAMPLE_INPUT) == 114
+    assert solve_part1(PUZZLE_INPUT) == 2174807968
 
 
 def test_part2():
-    assert solve_part2(EXAMPLE_INPUT) == None
-    assert solve_part2(PUZZLE_INPUT) == None
+    assert solve_part2(EXAMPLE_INPUT) == 2
+    assert solve_part2(PUZZLE_INPUT) == 1208
 
 
 PUZZLE_INPUT = parse_input(
@@ -17,4 +17,7 @@ PUZZLE_INPUT = parse_input(
 )
 
 EXAMPLE_INPUT = parse_input("""\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
 """)
