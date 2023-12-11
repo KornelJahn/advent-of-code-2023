887471c656fc12a1f25e457177d703a22e5ce89a
Kornel Jahn <kjahn.public@gmail.com>
2023-12-11 07:41:40 +0100

    Solve day11.

--- src/aoc2023/day11.py
+++ src/aoc2023/day11.py
@@ -1,10 +1,40 @@
+import itertools
+import re
+
+
 def parse_input(raw_input):
-    return None
+    image = raw_input.strip()
+    n = raw_input.index("\n") + 1
+    galaxies = [to_2d_indices(m.start(), n) for m in re.finditer("#", image)]
+    return galaxies
 
 
-def solve_part1(input_):
-    return None
+def solve_part1(galaxies):
+    return sum_of_distances(expand(galaxies, 2))
 
 
-def solve_part2(input_):
-    return None
+def solve_part2(galaxies, factor=1000000):
+    return sum_of_distances(expand(galaxies, factor))
+
+
+def to_2d_indices(flat_idx, row_length):
+    return flat_idx // row_length, flat_idx % row_length
+
+
+def sum_of_distances(galaxies):
+    return sum(dist(g1, g2) for g1, g2 in itertools.combinations(galaxies, 2))
+
+
+def expand(galaxies, factor):
+    # The row and column indices of empty empty rows and columns, respectively
+    vs, hs = (set(range(len(xs))) - set(xs) for xs in zip(*galaxies))
+    c = factor - 1
+    return [
+        (i + c * sum(v < i for v in vs), j + c * sum(h < j for h in hs))
+        for i, j in galaxies
+    ]
+
+
+def dist(g1, g2):
+    (i1, j1), (i2, j2) = g1, g2
+    return abs(i2 - i1) + abs(j2 - j1)  # Manhattan distance
--- tests/day11_test.py
+++ tests/day11_test.py
@@ -4,12 +4,13 @@ from aoc2023.day11 import parse_input, solve_part1, solve_part2
 
 def test_part1():
     assert solve_part1(EXAMPLE_INPUT) == 374
-    # assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(PUZZLE_INPUT) == 9693756
 
 
 def test_part2():
-    assert solve_part2(EXAMPLE_INPUT) == None
-    assert solve_part2(PUZZLE_INPUT) == None
+    assert solve_part2(EXAMPLE_INPUT, 10) == 1030
+    assert solve_part2(EXAMPLE_INPUT, 100) == 8410
+    assert solve_part2(PUZZLE_INPUT) == 717878258016
 
 
 PUZZLE_INPUT = parse_input(
