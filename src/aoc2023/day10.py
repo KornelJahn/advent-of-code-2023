d42f822121c9ac1e96ae4f1a018c6f7ea2627647
Kornel Jahn <kjahn.public@gmail.com>
2023-12-11 06:40:38 +0100

    Solve day10.

--- src/aoc2023/day10.py
+++ src/aoc2023/day10.py
@@ -1,10 +1,82 @@
+L, R, U, D = (0, -1), (0, 1), (-1, 0), (1, 0)
+TILES = {
+    "|": {U: U, D: D}, "-": {L: L, R: R},
+    "L": {D: R, L: U}, "J": {D: L, R: U},
+    "7": {U: L, R: D}, "F": {U: R, L: D},
+}
+STARTERS = {frozenset(dirmap.values()): tile for tile, dirmap in TILES.items()}
+
+
 def parse_input(raw_input):
-    return None
+    raw_input = raw_input.strip()
+    map_ = [list(row) for row in raw_input.strip().split("\n")]
+    i0, j0 = to_2d_indices(raw_input.index("S"), len(map_[0]) + 1)
+    map_[i0][j0] = find_out_tile(map_, i0, j0)
+    return map_, i0, j0
 
 
 def solve_part1(input_):
-    return None
+    path = trace_path(*input_)
+    return len(path) // 2
 
 
 def solve_part2(input_):
-    return None
+    map_, i0, j0 = input_
+    boundary = set(trace_path(map_, i0, j0))
+    area_counter = 0
+    for i, row in enumerate(map_):
+        for j, tile in enumerate(row):
+            if (i, j) in boundary:
+                continue
+            isect_count = sum(
+                1 for k in range(j+1, len(row))
+                if (i, k) in boundary and row[k] in "|F7"
+            )
+            if isect_count % 2 == 1:
+                area_counter += 1
+    return area_counter
+
+
+def to_2d_indices(flat_idx, row_length):
+    return flat_idx // row_length, flat_idx % row_length
+
+
+def find_out_tile(map_, i, j):
+    return STARTERS[
+        frozenset(
+            vec for vec in (L, U, R, D) if
+            step(map_, i, j, *vec)[2] is not None
+        )
+    ]
+
+
+def trace_path(map_, i0, j0):
+    tile = get_tile(map_, i0, j0)
+    p0, q0 = next(iter(TILES[tile].values()))
+    return [(i, j) for i, j in walk(map_, i0, j0, p0, q0)]
+
+
+def get_tile(map_, i, j):
+    if 0 <= i < len(map_) and 0 <= j < len(map_[0]):
+        return map_[i][j]
+    else:
+        return "."
+
+
+def walk(map_, i0, j0, p0, q0):
+    i, j, p, q = i0, j0, p0, q0
+    while True:
+        i, j, p, q = step(map_, i, j, p, q)
+        yield i, j
+        if (i == i0 and j == j0) or p is None:
+            break
+
+
+def step(map_, i, j, p, q):
+    i, j = i + p, j + q
+    tile = get_tile(map_, i, j)
+    try:
+        p, q = TILES[tile][p, q]
+    except KeyError:
+        p, q = None, None
+    return i, j, p, q
--- tests/day10_test.py
+++ tests/day10_test.py
@@ -3,23 +3,37 @@ from aoc2023.day10 import parse_input, solve_part1, solve_part2
 
 
 def test_part1():
-    assert solve_part1(EXAMPLE_INPUT) == 8
-    assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(EXAMPLE1_INPUT) == 8
+    assert solve_part1(PUZZLE_INPUT) == 6831
 
 
 def test_part2():
-    assert solve_part2(EXAMPLE_INPUT) == None
-    assert solve_part2(PUZZLE_INPUT) == None
+    assert solve_part2(EXAMPLE2_INPUT) == 10
+    assert solve_part2(PUZZLE_INPUT) == 305
 
 
 PUZZLE_INPUT = parse_input(
     (Path(__file__).parent / "data" / "day10.txt").read_text()
 )
 
-EXAMPLE_INPUT = parse_input("""\
+EXAMPLE1_INPUT = parse_input("""\
 ..F7.
 .FJ|.
 SJ.L7
 |F--J
 LJ...
+""")
+
+
+EXAMPLE2_INPUT = parse_input("""\
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
 """)
