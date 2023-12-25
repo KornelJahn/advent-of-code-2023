2a8d21cd7d38693ad1d035ea5d82e186a4f5e6bc
Kornel Jahn <kjahn.public@gmail.com>
2023-12-25 14:25:42 +0100

    Solve day16.

--- src/aoc2023/day16.py
+++ src/aoc2023/day16.py
@@ -1,10 +1,46 @@
+from collections import defaultdict
+
+ELEMENTS = {
+    ".": lambda vx, vy: {(vx, vy)},
+    "/": lambda vx, vy: {(vy, vx)},
+    "\\": lambda vx, vy: {(-vy, -vx)},
+    "|": lambda vx, vy: {(0, vy + vx*vx), (0, vy - vx*vx)},
+    "-": lambda vx, vy: {(vx + vy*vy, 0), (vx - vy*vy, 0)},
+}
+
+
 def parse_input(raw_input):
-    return None
+    rows = raw_input.strip().split("\n")
+    return rows
 
 
-def solve_part1(input_):
-    return None
+def solve_part1(rows):
+    return count_energized(rows, (0, 0, 1, 0))
 
 
-def solve_part2(input_):
-    return None
+def solve_part2(rows):
+    xmax, ymax = len(rows[0]) - 1, len(rows) - 1
+    initial_states = (
+        [(0, y, 1, 0) for y in range(-ymax, 1)]
+        + [(xmax, y, -1, 0) for y in range(-ymax, 1)]
+        + [(x, 0, 0, -1) for x in range(0, xmax + 1)]
+        + [(x, ymax, 0, 1) for x in range(0, xmax + 1)]
+    )
+    return max(count_energized(rows, state) for state in initial_states)
+
+
+def count_energized(rows, initial_state):
+    energized = defaultdict(set)
+    xmax, ymax = len(rows[0]) - 1, len(rows) - 1
+    stack = [initial_state]
+    while stack:
+        x, y, vx, vy = stack.pop()
+        if (vx, vy) in energized.get((x, y), set()):
+            continue
+        energized[x, y].add((vx, vy))
+        for vx, vy in ELEMENTS[rows[-y][x]](vx, vy):
+            xp = x + vx
+            yp = y + vy
+            if 0 <= xp <= xmax and -ymax <= yp <= 0:
+                stack.append((xp, yp, vx, vy))
+    return len(energized)
--- tests/day16_test.py
+++ tests/day16_test.py
@@ -4,20 +4,20 @@ from aoc2023.day16 import parse_input, solve_part1, solve_part2
 
 def test_part1():
     assert solve_part1(EXAMPLE_INPUT) == 46
-    assert solve_part1(PUZZLE_INPUT) == None
+    assert solve_part1(PUZZLE_INPUT) == 8249
 
 
 def test_part2():
-    assert solve_part2(EXAMPLE_INPUT) == None
-    assert solve_part2(PUZZLE_INPUT) == None
+    assert solve_part2(EXAMPLE_INPUT) == 51
+    assert solve_part2(PUZZLE_INPUT) == 8444
 
 
 PUZZLE_INPUT = parse_input(
     (Path(__file__).parent / "data" / "day16.txt").read_text()
 )
 
-EXAMPLE_INPUT = parse_input(r"""\
-.|...\....
+EXAMPLE_INPUT = parse_input(
+r""".|...\....
 |.-.\.....
 .....|-...
 ........|.
